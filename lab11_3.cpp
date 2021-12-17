7063368d104a77c1503f7e658b16ba2271034781
Fluke <hunterx52110@gmail.com>
2021-12-17 17:18:42 +0700

    lab11_3completed

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,35 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <iomanip>  
+#include <cstdlib>
+#include <string>
+#include <cmath>
+using namespace std;
+
+    int main(){
+        ifstream source;
+        source.open("score.txt");
+
+        int i=0;
+        string text;
+        double sum =0, xbar=0, n;
+        
+        
+        while(getline(source,text)){
+            n = atof(text.c_str());
+            sum += n;
+            xbar += pow(n,2);
+            i++;
+        }
+        double mean = sum/i,deviation;
+        deviation = sqrt((xbar/i)-pow(mean,2));
+        cout << "Number of data = " << i << endl;
+        cout << setprecision(3);
+        cout << "Mean = "<< mean << endl;
+        cout << "Standard deviation = "<< deviation;
+
+         source.close();
+
+
+
+    }
