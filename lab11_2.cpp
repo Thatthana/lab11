0dfc16288e19035cf08455c5e067cce13ad0061d
Fluke <hunterx52110@gmail.com>
2021-12-17 17:18:16 +0700

    lab11_2 completed

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -9,8 +9,14 @@ int main (){
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	string t;
+	dest << "-------------------- BOOM ---------------------" << endl;
+		while(getline(source,t)){
+
+		dest << t << endl;
+		}
+	dest << "-------------------- HA!! ---------------------";
+
 
     source.close();
     dest.close();
